ba3a0c108275a46a743df915043cbe61db09f0fb
arif-05 <arifcse949@gmail.com>
2024-10-22 22:57:21 +0600

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
 
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
- 
+ const [bookmarks,setBookmarks] = useState([])
+ const [readingTime, setReadingTime] = useState(0)
+
+ const handleAddToBookmark = blog =>{
+      const newBookmarks = [...bookmarks, blog]
+      setBookmarks(newBookmarks);
+
+ }
+
+ const handleMarkAsRead = (id,time) =>{
+    setReadingTime(readingTime + time);
+    // Remove bookmark from blog
+    // console.log('remove bookmark from blogs', id);
+
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
+    setBookmarks(remainingBookmarks)
+
+
+ }
+
 
   return (
     <>
-     
-
-      <Header></Header>
-      <div className='md:flex'>
-      <Blogs></Blogs>
-      <Bookmarks></Bookmarks>
+  
+      <Header ></Header>
+      <div className='md:flex container mx-auto'>
+      <Blogs
+       handleAddToBookmark = {handleAddToBookmark}
+       handleMarkAsRead = {handleMarkAsRead}
+    
+      ></Blogs>
+      <Bookmarks 
+      bookmarks = {bookmarks}
+      readingTime = {readingTime}
+      ></Bookmarks>
       </div>
       
     </>
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,46 @@
-import PropTypes from 'prop-types';
 
-const Blog = ({blog}) => {
-    console.log(blog)
+import PropTypes from 'prop-types';
+import { FaBookmark } from "react-icons/fa";
+
+const Blog = ({blog , handleAddToBookmark, handleMarkAsRead}) => {
+    const { id, title , cover, reading_time, author, posted_date, author_img , hash_tags } = blog;
     return (
-        <div>
+        <div className='my-20 space-y-4'>
+            <img className='w-full mb-6' src={cover} alt={` cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4 '>
+                <div className='flex gap-2 p-3'>
+                    <img className='w-12' src={author_img} alt="" />
+                    <div>
+                        <h3 className='text-2xl'> {author} </h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button 
+                    onClick={ () => handleAddToBookmark(blog)}
+                    className='ml-2 text-2xl text-red-500'> <FaBookmark></FaBookmark> </button>
+                </div>
+            </div>
             
+            
+            <h3 className="text-4xl">{title}</h3>
+            <p> 
+               {
+                hash_tags.map((hash,idx) => <span key={idx}> <a href=""> {hash}</a> </span> )
+               } 
+             </p>
+             <button onClick={ () => handleMarkAsRead( id, reading_time)  }
+             className='text-purple-500 font-bold underline'>Mark as read</button>
         </div>
+        
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired ,
+    handleAddToBookmark: PropTypes.func.isRequired,
+    handleMarkAsRead: PropTypes.func
 }
 
 export default Blog;
